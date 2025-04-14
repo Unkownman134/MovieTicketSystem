42f2e7c4556a6100dfcfbdc8690f74cc69bffc61
God_of_Admin <O0ohasho0O@proton.me>
2025-04-14 17:07:49 +0800

    主界面登录

--- src/main/java/com/movieticket/gongding/view/Main.java
+++ src/main/java/com/movieticket/gongding/view/Main.java
@@ -12,12 +12,14 @@ public class Main {
     public void start() {
         while (true) {
             System.out.println("\n=== 电影售票系统 ===");
+            System.out.println("1. 用户登录");
             System.out.println("2. 用户注册");
             System.out.println("0. 退出系统");
             System.out.print("请选择操作：");
 
             switch (scanner.nextLine()) {
                 case "1":
+                    userLogin();
                     break;
                 case "2":
                     userRegister();
@@ -30,6 +32,31 @@ public class Main {
                 default:
                     System.out.println("无效选择，请重新输入！");
             }
+        }
+    }
+
+    private void userLogin() {
+        System.out.print("请输入用户名：");
+        String username = scanner.nextLine();
+        System.out.print("请输入密码：");
+        String password = scanner.nextLine();
+
+        int result = userService.login(username, password);
+        switch (result) {
+            case 1:
+                //TODO：用户登录界面
+                break;
+            case -1:
+                System.out.println("用户不存在！");
+                break;
+            case -2:
+                System.out.println("用户已被拉黑！");
+                break;
+            case -3:
+                System.out.println("密码错误！");
+                break;
+            default:
+                System.out.println("登录失败！");
         }
     }
 
